16ef156b6540bc580ef690985676bb1ee5aaae71
adolfo-ab <adolfoab6@gmail.com>
2024-04-03 19:51:07 +0200

    Add function to separate features and labels and split into training and testing sets

--- data.go
+++ data.go
@@ -4,23 +4,20 @@ import (
 	"bufio"
 	"encoding/csv"
 	"fmt"
+	"math/rand"
 	"os"
 	"strconv"
 )
 
 const fileName = "./data/iris.data"
+const trainSize = 0.8
 
 func loadData() ([][]string, error) {
 	csvFile, err := os.Open(fileName)
 	if err != nil {
 		return nil, err
 	}
-	defer func(csvFile *os.File) {
-		err := csvFile.Close()
-		if err != nil {
-			panic(`Couldn't close file`)
-		}
-	}(csvFile)
+	defer csvFile.Close()
 
 	reader := csv.NewReader(bufio.NewReader(csvFile))
 	lines, err := reader.ReadAll()
@@ -59,4 +56,27 @@ func parseData(data [][]string) ([][]float64, error) {
 	}
 
 	return parsedData, nil
+}
+
+func splitData(data [][]float64) ([][]float64, []float64, [][]float64, []float64) {
+	// Shuffle the data
+	rand.Shuffle(len(data), func(i, j int) {
+		data[i], data[j] = data[j], data[i]
+	})
+
+	// Separate features and labels
+	features := make([][]float64, len(data))
+	labels := make([]float64, len(data))
+
+	for i, row := range data {
+		features[i] = row[:len(row)-1]
+		labels[i] = row[len(row)-1]
+	}
+
+	// Split into training and testing sets
+	splitIndex := int(float64(len(data)) * trainSize)
+	trainFeatures, testFeatures := features[:splitIndex], features[splitIndex:]
+	trainLabels, testLabels := labels[:splitIndex], labels[splitIndex:]
+
+	return trainFeatures, trainLabels, testFeatures, testLabels
 }
--- main.go
+++ main.go
@@ -13,5 +13,25 @@ func main() {
 		panic("Oh no!")
 	}
 
-	fmt.Println(parsedData)
+	trainFeatures, trainLabels, testFeatures, testLabels := splitData(parsedData)
+
+	fmt.Println("Training features: ")
+	fmt.Println(trainFeatures)
+	fmt.Println("Length of training features: ")
+	fmt.Println(len(trainFeatures))
+
+	fmt.Println("Training labels: ")
+	fmt.Println(trainLabels)
+	fmt.Println("Length of training labels: ")
+	fmt.Println(len(trainLabels))
+
+	fmt.Println("Testing features: ")
+	fmt.Println(testFeatures)
+	fmt.Println("Length of testing features: ")
+	fmt.Println(len(testFeatures))
+
+	fmt.Println("Testing labels: ")
+	fmt.Println(testLabels)
+	fmt.Println("Length of testing labels: ")
+	fmt.Println(len(testLabels))
 }
