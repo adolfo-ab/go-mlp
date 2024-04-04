8e6d6c8db353aa917c6006bf8236a41e6db2b1d7
adolfo-ab <adolfoab6@gmail.com>
2024-04-04 19:30:12 +0200

    Use one-hot encoding for labels

--- data.go
+++ data.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"encoding/csv"
-	"fmt"
 	"math/rand"
 	"os"
 	"strconv"
@@ -28,55 +27,45 @@ func loadData() ([][]string, error) {
 	return lines, nil
 }
 
-func parseData(data [][]string) ([][]float64, error) {
-	parsedData := make([][]float64, len(data))
-	speciesMap := map[string]float64{
-		"Iris-setosa":     1.0,
-		"Iris-versicolor": 2.0,
-		"Iris-virginica":  3.0,
-	}
-
-	for i, row := range data {
-		parsedRow := make([]float64, len(row))
-		for j := range row {
-			if j == len(row)-1 {
-				species := row[j]
-				if val, ok := speciesMap[species]; ok {
-					parsedRow[j] = val
-				} else {
-					return nil, fmt.Errorf("unknown species: %s", species)
-				}
-			} else {
-				val, err := strconv.ParseFloat(row[j], 64)
-				if err != nil {
-					return nil, err
-				}
-				parsedRow[j] = val
-			}
-		}
-		parsedData[i] = parsedRow
-	}
-
-	return parsedData, nil
-}
-
-func splitData(data [][]float64) ([][]float64, []float64, [][]float64, []float64) {
+func parseData(data [][]string) ([][]float64, [][]float64, error) {
 	// Shuffle the data
 	rand.Shuffle(len(data), func(i, j int) {
 		data[i], data[j] = data[j], data[i]
 	})
 
-	// Separate features and labels
+	// Parse data, splitting into features and one-hot encoded labels in the process
 	features := make([][]float64, len(data))
-	labels := make([]float64, len(data))
+	labels := make([][]float64, len(data))
 
 	for i, row := range data {
-		features[i] = row[:len(row)-1]
-		labels[i] = row[len(row)-1]
-	}
+		features[i] = make([]float64, len(row)-1)
+		labels[i] = make([]float64, 3)
 
+		for j, value := range row {
+			if j == len(row)-1 {
+				switch value {
+				case "Iris-setosa":
+					labels[i][0] = 1
+				case "Iris-versicolor":
+					labels[i][1] = 1
+				case "Iris-virginica":
+					labels[i][2] = 1
+				}
+			} else {
+				if val, err := strconv.ParseFloat(value, 64); err != nil {
+					return nil, nil, err
+				} else {
+					features[i][j] = val
+				}
+			}
+		}
+	}
+	return features, labels, nil
+}
+
+func splitData(features [][]float64, labels [][]float64) ([][]float64, [][]float64, [][]float64, [][]float64) {
 	// Split into training and testing sets
-	splitIndex := int(float64(len(data)) * trainSize)
+	splitIndex := int(float64(len(features)) * trainSize)
 	trainFeatures, testFeatures := features[:splitIndex], features[splitIndex:]
 	trainLabels, testLabels := labels[:splitIndex], labels[splitIndex:]
 
