131f257059b5092ca89367beedc5217988e0275b
adolfo-ab <adolfoab6@gmail.com>
2024-04-04 08:48:00 +0200

    Implement NeuralNetwork builder and activation functions

--- network.go
+++ network.go
@@ -1,6 +1,9 @@
 package main
 
-import "gonum.org/v1/gonum/mat"
+import (
+	"gonum.org/v1/gonum/mat"
+	"math"
+)
 
 type NeuralNetwork struct {
 	config  NeuralNetworkConfig
@@ -16,4 +19,33 @@ type NeuralNetworkConfig struct {
 	numOutputNeurons int
 	numEpochs        int
 	learningRate     float64
+}
+
+func NewNeuralNetwork(config NeuralNetworkConfig) *NeuralNetwork {
+	return &NeuralNetwork{config: config}
+}
+
+func relu(x float64) float64 {
+	return math.Max(0, x)
+}
+
+func reluDerivative(x float64) float64 {
+	if x > 0 {
+		return 1
+	}
+	return 0
+}
+
+func softmax(x []float64) []float64 {
+	sumExp := 0.0
+	out := make([]float64, len(x))
+
+	for _, value := range x {
+		sumExp += math.Exp(value)
+	}
+
+	for i, value := range x {
+		out[i] = math.Exp(value) / sumExp
+	}
+	return out
 }
